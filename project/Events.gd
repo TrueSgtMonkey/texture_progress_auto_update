46825ba7b4765ed988a8d3bcaeb1b0ea8927b282
Cody Thompson <47369629+TrueSgtMonkey@users.noreply.github.com>
2021-02-19 01:33:14 -0800

    Revert "fsdsfa"
    This reverts commit 18fdea47fde81a8d4fa98ef4dfd6163618716239.

--- project/Events.gd
+++
@@ -1,3 +0,0 @@
-extends Node
-
-signal power_changed(power)
--- project/scenes/TextureProgress.gd
+++
@@ -1,7 +0,0 @@
-extends TextureProgress
-
-func _ready():
-	Events.connect("power_changed", self, "changePower")
-	
-func changePower(power : int):
-	value = power
