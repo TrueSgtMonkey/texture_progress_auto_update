722e6da6371785408fcc103561789101d6e1b562
Cody Thompson <47369629+TrueSgtMonkey@users.noreply.github.com>
2021-02-18 23:51:16 -0800

    Revert "project_end"
    This reverts commit 5c610c89210dade77f9680c392bf6f729e2e45b5.

--- project/Globals.gd
+++ project/Globals.gd
@@ -1,7 +1,3 @@
 extends Node
 
 var shot := false
-
-func _input(event):
-	if event.is_action_pressed("end_game"):
-		get_tree().quit()
--- project/scenes/HBoxContainer.gd
+++
@@ -1,7 +0,0 @@
-extends HBoxContainer
-
-func _ready():
-	Events.connect("power_changed", self, "updateTexture")
-
-func updateTexture(power : int):
-	$TextureProgress.value = power
